7df48ded23c5cf6b4f48eab908b474499dca3d9c
Christian Arroyo <ChristianArroyo308@yahoo.com>
2022-09-25 14:19:49 -0700

    models update

--- system/models.py
+++ system/models.py
@@ -18,3 +18,50 @@ class User(db.Model, UserMixin):
     category1 = db.Column(db.String(15), nullable=False)
     category2 = db.Column(db.String(15), nullable=False)
     token = db.Column(db.String, default='', unique=True)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __init__(self, username, email, password, category1, category2, token=''):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.category1 = category1
+        self.category2 = category2
+        self.token = self.set_token(20)
+
+    def set_token(self, length): 
+        return secrets.token_hex(length)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable=False)
+
+    def __init__(self, title, content, author, user_token):
+        self.title = title
+        self.content = content
+        self.author = author
+        self.user_token = user_token
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ['id', 'username', 'email']
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+class PostSchema(ma.Schema):
+    class Meta:
+        fields = ['id', 'title', 'content']
+
+post_schema = PostSchema()
+posts_schema = PostSchema(many=True)
