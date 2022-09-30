312a8ec358dbc270f8cb67a19c6d908562ded284
Christian Arroyo <ChristianArroyo308@yahoo.com>
2022-09-29 20:30:56 -0700

    update. progress on homepage. Working toward MVP

--- system/routes.py
+++ system/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request, abort, jsonify
 from system import app, db, bcrypt
-# from system.forms import RegistrationForm, AccountForm, LoginForm, PostForm
+from system.forms import RegistrationForm, AccountForm, LoginForm, PostForm
 from system.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -8,3 +8,18 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/home")
 def home():
     return render_template('home.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password')
+    return render_template('login.html', form=form)
